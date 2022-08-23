4c46d247542f12d1cc6d92d679476ef36e60e358
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-08-22 22:27:57 -0300

    Adição de testes para criação de usuarios

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,60 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+import { AppError } from "src/shared/errors/AppError"
+import {CreateUserError} from "./CreateUserError"
+let createUserUseCase: CreateUserUseCase
+let inMemoryUsersRepository: InMemoryUsersRepository
+
+describe("Create User", () => {
+
+
+  beforeAll(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+  })
+
+  it("shoud be able to create a new user", async () => {
+
+    const user = {
+      name: "test",
+      email: "test@test.com",
+      password:"test"
+    }
+
+
+    await createUserUseCase.execute({
+      name: user.name,
+      email: user.email,
+      password: user.password
+    })
+
+    const userCreate = await inMemoryUsersRepository.findByEmail(user.email)
+
+    expect(userCreate).toHaveProperty("id")
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+
+    expect(async () => {
+
+        const user = {
+          name: "test",
+          email: "test@test.com",
+          password:"test"
+        }
+
+        await createUserUseCase.execute({
+          name: user.name,
+          email: user.email,
+          password: user.password
+        })
+
+        await createUserUseCase.execute({
+          name: user.name,
+          email: user.email,
+          password: user.password
+        })
+
+    }).rejects.toBeInstanceOf(CreateUserError)
+  })
+})
